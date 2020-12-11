a016ebb7ea6d712fd48492af7966736b2707f5bf
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-11 22:59:04 +0530

    [Prudhvi] refractored

--- UserRegistrationTest/UserValidator.cs
+++ UserRegistrationTest/UserValidator.cs
@@ -9,6 +9,15 @@ namespace UserRegistrationTest
         //Constants
         public const string REGEX_NAME = "^[A-Z][a-zA-Z]{2,}";
         /// <summary>
+        /// Validates the first name.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <returns></returns>
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX_NAME);
+        }
+        /// <summary>
         /// Validates the last name.
         /// </summary>
         /// <param name="lastName">The last name.</param>
