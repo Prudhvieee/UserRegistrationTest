1ffceef80aa6cee9285b7505fcf6d08745f49aa5
Prudhvieee <biyyappu.prudhvi@gmail.com>
2020-12-11 22:41:06 +0530

    Add project files.

---
+++ UserRegistrationTest/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace UserRegistrationTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to user Registration!");
+        }
+    }
+}
